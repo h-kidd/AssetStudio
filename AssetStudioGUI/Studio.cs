cfb217cd5b137924b2ac483216288b3a29114c00
Hayden <46706006+h-kidd@users.noreply.github.com>
2023-02-15 23:24:35 +0000

    Change fullPath to originalPath

--- AssetStudioGUI/Studio.cs
+++ AssetStudioGUI/Studio.cs
@@ -485,7 +485,7 @@ namespace AssetStudioGUI
                                         new XElement("Container", asset.Container),
                                         new XElement("Type", new XAttribute("id", (int)asset.Type), asset.TypeString),
                                         new XElement("PathID", asset.m_PathID),
-                                        new XElement("Source", asset.SourceFile.fullName),
+                                        new XElement("Source", asset.SourceFile.originalPath),
                                         new XElement("Size", asset.FullSize)
                                     )
                                 )
